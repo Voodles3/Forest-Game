ce12bdef32bddbae85bd413c887b6226bd2ed67c
Voodles3 <voodles10@gmail.com>
2024-05-31 03:59:08 -0700

    Added jumping and lots of movement changes
    Added jumping, better control on ground and in air using dynamic drag and gravity, and tons of adjustable values for tweaking movement.

--- Sewer Game/Assets/Scripts/PlayerLook.cs
+++ Sewer Game/Assets/Scripts/PlayerLook.cs
@@ -4,17 +4,25 @@ using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 {
-    public Transform orientation;
-    public InputAction lookAction;
-
     public float sensitivity = 100f;
     public float smoothTime = 0.1f;
+
+    public Transform orientation;
+
+    PlayerActions inputActions;
+    InputAction lookAction;
 
     Vector2 currentMouseDelta;
     Vector2 currentMouseDeltaVelocity;
 
     float pitch = 0f;
     float yaw = 0f;
+
+    void Awake()
+    {
+        inputActions = new PlayerActions();
+        lookAction = inputActions.Camera.Look;
+    }
 
     void Start()
     {
@@ -26,18 +34,13 @@ public class MouseLook : MonoBehaviour
         Vector2 targetMouseDelta = lookAction.ReadValue<Vector2>() * sensitivity * Time.fixedUnscaledDeltaTime;
 
         // Smooth the mouse delta
-        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, smoothTime);
-
-        //Debug.Log($"Current mouse delta: {currentMouseDelta}");
+        //currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, smoothTime);
 
         //float mouseX = currentMouseDelta.x;
         //float mouseY = currentMouseDelta.y;
 
         float mouseX = targetMouseDelta.x;
         float mouseY = targetMouseDelta.y;
-
-        //float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity;
-        //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity;
 
         yaw += mouseX;
 
@@ -48,13 +51,13 @@ public class MouseLook : MonoBehaviour
         orientation.rotation = Quaternion.Euler(0f, yaw, 0f);
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
-        lookAction.Enable();
+        inputActions.Camera.Enable();
     }
 
-    private void OnDisable()
+    void OnDisable()
     {
-        lookAction.Disable();
+        inputActions.Camera.Disable();
     }
 }
--- Sewer Game/Assets/Scripts/PlayerMovement.cs
+++ Sewer Game/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.ComponentModel;
 using System.Linq.Expressions;
 using TMPro;
 using UnityEngine;
@@ -6,52 +8,187 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float groundDrag;
+    [SerializeField] float airDrag;
+    [SerializeField] float dragTransitionTime;
+    bool dragTransitioning;
+    bool canAccelerate = true;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airSpeedMultiplier;
+    [SerializeField] float jumpGravityMultiplier;
+    bool readyToJump = true;
+
+    [Header("Ground Check")]
+    public LayerMask groundMask;
+    [SerializeField] float groundDistance;
+    [SerializeField] [ReadOnly(true)] bool grounded;
 
     public Transform orientation;
+    public Transform playerBody;
     public TMP_Text velocityText;
-    public InputAction moveAction;
+
+    PlayerActions inputActions;
+    InputAction jumpAction;
 
     Rigidbody rb;
     Vector3 moveDirection;
     Vector2 inputs;
 
+    void Awake()
+    {
+        inputActions = new PlayerActions();
+        jumpAction = inputActions.Gameplay.Jump;
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        rb.drag = groundDrag;
     }
 
     void Update()
     {
-        velocityText.text = $"Velocity: {rb.velocity.magnitude:F2}";
         ReceiveInput();
+        GroundCheck();
+        LimitSpeed();
+        ApplyDrag();
+
+        Vector3 horizVel = new(rb.velocity.x, 0, rb.velocity.z);
+        velocityText.text = $"Velocity: {horizVel.magnitude:F2}";
     }
 
     void FixedUpdate() 
     {
         MovePlayer();
+        ApplyGravity();
     }
 
     void ReceiveInput()
     {
-        inputs = moveAction.ReadValue<Vector2>();
+        inputs = inputActions.Gameplay.Movement.ReadValue<Vector2>();
+        
+        if (jumpAction.ReadValue<float>() > 0)
+        {
+            OnJump();
+        }
     }
 
     void MovePlayer()
     {
+        Vector3 moveForce;
         moveDirection = orientation.forward * inputs.y + orientation.right * inputs.x;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 100f * Time.deltaTime, ForceMode.Force);
+        if (grounded)
+        {
+            moveForce = 350f * moveSpeed * Time.deltaTime * moveDirection.normalized;
+        }
+        else
+        {
+            moveForce = 350f * airSpeedMultiplier * moveSpeed * Time.deltaTime * moveDirection.normalized;
+        }
+        if (canAccelerate) { rb.AddForce(moveForce, ForceMode.Force); }
+    }
+
+    void OnJump()
+    {
+        if (readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ApplyGravity()
+    {
+        // Jumping gravity
+        if (!grounded)
+        {
+            rb.AddForce(Physics.gravity * jumpGravityMultiplier, ForceMode.Force);
+        }
+    }
+
+    void GroundCheck()
+    {
+        if (Physics.Raycast(playerBody.transform.position, Vector3.down, groundDistance, groundMask))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    void ApplyDrag()
+    {
+        if (grounded && Mathf.Abs(rb.velocity.y) < 0.1f)
+        {
+            if (rb.drag == airDrag)
+            {
+                StartCoroutine(SmoothDrag());
+            }
+        }
+        else
+        {
+            rb.drag = airDrag;
+            StopAllCoroutines();
+        }
+    }
+
+    IEnumerator SmoothDrag()
+    {
+        float timeElapsed = 0f;
+        dragTransitioning = true;
+        while (timeElapsed <= dragTransitionTime)
+        {
+            rb.drag = Mathf.Lerp(airDrag, groundDrag, timeElapsed / dragTransitionTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        rb.drag = groundDrag;
+        dragTransitioning = false;
+    }
+
+    void LimitSpeed()
+    {
+        Vector3 horizVel = new(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (horizVel.magnitude > moveSpeed && grounded)
+        {
+            canAccelerate = false;
+            //Vector3 correctedVel = horizVel.normalized * moveSpeed;
+            //rb.velocity = new(correctedVel.x, rb.velocity.y, correctedVel.z);
+        }
+        else
+        {
+            canAccelerate = true;
+        }
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
     }
 
     void OnEnable()
     {
-        moveAction.Enable();
+        inputActions.Gameplay.Enable();
     }
 
     void OnDisable()
     {
-        moveAction.Disable();
+        inputActions.Gameplay.Disable();
     }
 }
